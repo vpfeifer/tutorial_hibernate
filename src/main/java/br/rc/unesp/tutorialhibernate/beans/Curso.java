2a4b98acc8accf6040aa2c55dfd46e9ff0d860dd
Vinicius Pfeifer <vpfeifer20@gmail.com>
2016-05-18 23:26:00 -0300

    annotations

--- src/main/java/br/rc/unesp/tutorialhibernate/beans/Curso.java
+++ src/main/java/br/rc/unesp/tutorialhibernate/beans/Curso.java
@@ -1,7 +1,19 @@
 package br.rc.unesp.tutorialhibernate.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CURSO")
 public class Curso {
+    @Id @GeneratedValue
+    @Column(name = "ID")
     private long id;
+    
+    @Column(name = "NOME")
     private String nome;
 
     public Curso() {
